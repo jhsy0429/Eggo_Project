7629ffae3e50214363ad5a7f2a86425a9da15cd6
serin <tpflsgkdl@naver.com>
2022-05-11 12:19:18 +0900

    add camera

--- app/src/main/java/com/example/eggo_project/AllFragment.java
+++ app/src/main/java/com/example/eggo_project/AllFragment.java
@@ -1,5 +1,6 @@
 package com.example.eggo_project;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +10,47 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+
 public class AllFragment extends Fragment {
+
+    private LineChart chart;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_all, container, false);
+
+
+        chart = rootView.findViewById(R.id.all_linechart);
+
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+
+            float val = (float) (Math.random() * 10);
+            values.add(new Entry(i, val));
+        }
+
+        LineDataSet set1;
+        set1 = new LineDataSet(values, "DataSet 1");
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1); // add the data sets
+
+        // create a data object with the data sets
+        LineData data = new LineData(dataSets);
+
+        // black lines and points
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.BLACK);
+
+        // set data
+        chart.setData(data);
 
         return rootView;
     }
--- app/src/main/java/com/example/eggo_project/RegistrationActivity.java
+++ app/src/main/java/com/example/eggo_project/RegistrationActivity.java
@@ -1,16 +1,57 @@
 package com.example.eggo_project;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RegistrationActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btn_camera, btn_photo, btn_reg;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill_reg);
 
+        btn_camera = (Button)findViewById(R.id.btn_camera);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        btn_camera.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            // 촬영하기버튼 클릭 이벤트
+            case R.id.btn_camera:
+                // 카메라 기능을 Intent
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i, 0);
+                break;
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 카메라 촬영을 하면 이미지뷰에 사진 삽입
+        if(requestCode == 0 && resultCode == RESULT_OK) {
+            // Bundle로 데이터를 입력
+            Bundle extras = data.getExtras();
+
+            // Bitmap으로 컨비전
+            Bitmap imageBitmap = (Bitmap)extras.get("data");
+
+            // 이미지뷰에 Bitmap으로 이미지를 입력
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
